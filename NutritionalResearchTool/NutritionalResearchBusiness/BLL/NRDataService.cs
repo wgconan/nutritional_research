485ce4010660b24a8262270ae09362ed249ac7e9
wgconan <wgconan@msn.com>
2016-10-25 01:05:43 +0800

    完善项目构建

--- NutritionalResearchTool/NutritionalResearchBusiness/BLL/NRDataService.cs
+++ NutritionalResearchTool/NutritionalResearchBusiness/BLL/NRDataService.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace NutritionalResearchBusiness
+namespace NutritionalResearchBusiness.BLL
 {
-    public class Class1
+    public class NRDataService:INRDataService
     {
     }
 }
